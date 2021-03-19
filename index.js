195805ec6b6dc233340f3d06faaff4757a1b5066
Amir Hachani <79195577+amirhachani@users.noreply.github.com>
2021-03-19 19:36:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+/*$(".pad1").click(function() {
+  var audio = $(".sound1");
+  audio.play();
+});
+
+$("pad1").click(function(){
+  var audio = $(".sound")[0];
+  audio.play();
+})
+
+$("pad2").click(function(){
+  $('.sound2').play()
+})
+
+$("pad3").click(function(){
+  $('#sound3').play()
+})
+
+$("pad4").click(function(){
+  $('#sound4').play()
+})
+
+$("pad5").click(function(){
+  $('#sound5').play()
+})
+
+$("pad6").click(function(){
+  $('#sound6').play()
+})
+*/
+
+
+  
